222375d545af7ef54b4e6e1299802a7b5e46f39f
UltimateHikari <andy@andystpad.localdomain>
2021-03-18 11:21:13 +0700

    smoother (?) shadow translations; mb need to tune duration

--- src/main/java/com/hikari/hellofx/Views/EntityShadowView.java
+++ src/main/java/com/hikari/hellofx/Views/EntityShadowView.java
@@ -5,21 +5,27 @@ import com.hikari.hellofx.Base.IModelSubscriber;
 import com.hikari.hellofx.Entities.EntityShadow;
 
 import javafx.scene.paint.Color;
+import javafx.animation.TranslateTransition;
 import javafx.geometry.Point2D;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 public class EntityShadowView extends Circle implements IModelSubscriber{
 
 	public EntityShadowView() {
-		super(100,100,40);
+		super(0,0,40);
 		setFill(Color.ALICEBLUE);
 	}
 	
 	@Override
 	public void ModelChanged(BaseModel model) {
 		Point2D point =((EntityShadow)model).getPosition();
-		setCenterX(point.getX());
-		setCenterY(point.getY());
+		TranslateTransition tt = new TranslateTransition(Duration.millis(10), this);
+		tt.setToX(point.getX());
+		tt.setToY(point.getY());
+		tt.play();
+//		setCenterX(point.getX());
+//		setCenterY(point.getY());
 	}
 
 }
